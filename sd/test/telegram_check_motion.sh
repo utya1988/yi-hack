42a4fa76ad9f443387ae5d9f50ef7a644ac7f398
Gennady <utya1988@gmail.com>
2017-03-05 21:41:32 +0300

    add curl with lib, add telegram script

---
+++ sd/test/telegram_check_motion.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# each minute, check for a new video file (which is created in case of motion detection)
+# and if found, create the appropriate file for the http server
+
+API_TOKEN='API Token'
+
+CHAT_ID_1='Chat ID'
+CHAT_ID_2='Chat ID'
+DIR=/home/hd1/record/
+
+cd /home/hd1/record/
+old_motion_file=1
+new_motion_file=1
+
+while [ 1 -eq 1 ] 
+  do
+    motion_file=$(find . -type f -name "*.mp4" -mmin -1 | tail -1)
+    echo "M="$motion_file
+    echo $motion_file | sed "s/.\//record\//" > /home/hd1/test/http/motion
+	
+
+send_motion_file=$(echo $motion_file  | sed "s/.\//\/home\/hd1\/record\//")
+
+if [[ "$motion_file" = "$old_motion_file" ]];  then
+	echo "Строки одинаковые"
+##	
+	else 
+	echo "Строки не одинаковые"
+	#/home/curl --insecure --data "chat_id=$CHAT_ID_1&text=Moving detection" https://api.telegram.org/bot$API_TOKEN/sendMessage > /dev/null
+	
+	#/home/curl --insecure --data "chat_id=$CHAT_ID_2&text=Moving detection" https://api.telegram.org/bot$API_TOKEN/sendMessage > /dev/null
+		
+		/home/curl --insecure  -F document=@$send_motion_file  https://api.telegram.org/bot$API_TOKEN/sendDocument?chat_id=$CHAT_ID_1 > /dev/null
+		
+		/home/curl --insecure  -F document=@$send_motion_file  https://api.telegram.org/bot$API_TOKEN/sendDocument?chat_id=$CHAT_ID_2 > /dev/null
+		
+	old_motion_file=$motion_file;
+fi
+    sleep 5
+done
